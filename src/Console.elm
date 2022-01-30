2431caceb89388d473a7a66f45d80203c4395ade
pravdomil <pravdomil.toman@gmail.com>
2022-01-31 00:20:17 +0100

    console time functions

--- src/Console.elm
+++ src/Console.elm
@@ -14,3 +14,53 @@ log a =
     JavaScript.run "console.log(a)"
         (Json.Encode.string a)
         (Json.Decode.succeed a)
+
+
+time : String -> Task.Task JavaScript.Error ()
+time a =
+    JavaScript.run "console.time(a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
+
+
+timeEnd : String -> Task.Task JavaScript.Error ()
+timeEnd a =
+    JavaScript.run "console.timeEnd(a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
+
+
+timeTask : (JavaScript.Error -> x) -> String -> Task.Task x a -> Task.Task x a
+timeTask toError label a =
+    time label
+        |> Task.mapError toError
+        |> Task.andThen
+            (\_ ->
+                a
+                    |> taskAndThenWithResult
+                        (\v ->
+                            timeEnd label
+                                |> Task.mapError toError
+                                |> Task.andThen
+                                    (\_ ->
+                                        case v of
+                                            Ok v2 ->
+                                                Task.succeed v2
+
+                                            Err v2 ->
+                                                Task.fail v2
+                                    )
+                        )
+            )
+
+
+
+--
+
+
+taskAndThenWithResult : (Result x a -> Task.Task y b) -> Task.Task x a -> Task.Task y b
+taskAndThenWithResult next a =
+    a
+        |> Task.map Ok
+        |> Task.onError (Err >> Task.succeed)
+        |> Task.andThen next
