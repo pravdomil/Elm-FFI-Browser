c962c81bbff56df26ec2629092a69f8727e61c2e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-01-31 00:06:22 +0100

    fix docs

--- src/Console.elm
+++ src/Console.elm
@@ -1,13 +1,14 @@
 module Console exposing (..)
 
+{-| <https://developer.mozilla.org/en-US/docs/Web/API/console>
+-}
+
 import JavaScript
 import Json.Decode
 import Json.Encode
 import Task
 
 
-{-| <https://developer.mozilla.org/en-US/docs/Web/API/console>
--}
 log : String -> Task.Task JavaScript.Error String
 log a =
     JavaScript.run "console.log(a)"
