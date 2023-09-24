9a54ad20530844a16ca407c93b60d4e4e51b6c94
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-24 12:30:00 +0200

    next

--- src/Browser/Extra.elm
+++ src/Browser/Extra.elm
@@ -12,17 +12,3 @@ openInNewWindow a =
     JavaScript.run "window.open(a, '_blank')"
         (Json.Encode.string a)
         (Json.Decode.succeed ())
-
-
-{-| Gets patched by elm-ffi.
--}
-safePushUrl : Browser.Navigation.Key -> String -> Cmd msg
-safePushUrl =
-    Browser.Navigation.pushUrl
-
-
-{-| Gets patched by elm-ffi.
--}
-safeReplaceUrl : Browser.Navigation.Key -> String -> Cmd msg
-safeReplaceUrl =
-    Browser.Navigation.replaceUrl
