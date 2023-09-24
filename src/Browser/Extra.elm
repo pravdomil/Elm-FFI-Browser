3dc69a383f7db734d2d419f0867cad68cea2988e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-24 12:30:30 +0200

    next

--- src/Browser/Extra.elm
+++ src/Browser/Extra.elm
@@ -12,3 +12,17 @@ openInNewWindow a =
     JavaScript.run "window.open(a, '_blank')"
         (Json.Encode.string a)
         (Json.Decode.succeed ())
+
+
+pushUrl : Browser.Navigation.Key -> String -> Task.Task JavaScript.Error ()
+pushUrl _ a =
+    JavaScript.run "history.pushState({}, '', a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
+
+
+replaceUrl : Browser.Navigation.Key -> String -> Task.Task JavaScript.Error ()
+replaceUrl _ a =
+    JavaScript.run "history.replaceState({}, '', a)"
+        (Json.Encode.string a)
+        (Json.Decode.succeed ())
